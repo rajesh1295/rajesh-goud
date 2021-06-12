3eff60241cebca57caa522f0958b3864e0d0f8d8
Rajesh goud <rajeshgoud11295@gmail.com>
2021-06-12 19:14:11 +0530

    Project Created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Welcome from './components/welcome'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Welcome />
+      
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
